b1ae42141ee1896d16fc1ff5dde7e2eceb1d6da8
Jttanner <jonttanner@gmail.com>
2018-02-08 12:55:34 -0700

    addd cmake

--- milestone1/main.c
+++ milestone1/main.c
@@ -14,20 +14,6 @@
 
 int main(){
     queue_runTest();
-    queue_t * myQueue;
-    queue_init(&myQueue, SIZE, NAME);
-    queue_push(&myQueue, 10);
-    queue_push(&myQueue, 11);
-    queue_push(&myQueue, 12);
-    queue_push(&myQueue, 13);
-    queue_push(&myQueue, 14);
-    queue_print(&myQueue);
-    queue_pop(&myQueue);
-    queue_pop(&myQueue);
-    queue_pop(&myQueue);
-    queue_print(&myQueue);
-    queue_push(&myQueue, 15);
-    queue_print(&myQueue);
     return 0;
 }
 
