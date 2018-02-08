5dab7cd711977b65e30080b03796c1cea704a61c
Jttanner <jonttanner@gmail.com>
2018-02-08 13:39:22 -0700

    working dependencies

---
+++ filter.c
@@ -0,0 +1,5 @@
+//
+// Created by jontt on 2/8/2018.
+//
+
+#include "filter.h"
---
+++ filter.h
@@ -0,0 +1,19 @@
+/*
+ * filterTest.h
+ *
+ *  Created on: Mar 11, 2015
+ *      Author: hutch
+ */
+
+#ifndef FILTERTEST_H_
+#define FILTERTEST_H_
+
+#include <w32api/vss.h>
+
+// Invoke init before calling filterTest_runTest().
+void filterTest_init();
+
+// Performs a comprehensive test of the FIR, IIR filters and plots frequency response on the TFT.
+bool filterTest_runTest();
+
+#endif /* FILTERTEST_H_ */
