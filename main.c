15694baee6b80913586798db7023563438945f0e
Jttanner <jonttanner@gmail.com>
2018-03-08 12:40:08 -0700

    milestone 3 task 3 start

--- main.c
+++ main.c
@@ -1,49 +1,17 @@
-/*
- * main.c
- *
- *  Created on: Jan 11, 2018
- *      Author: tshumwa2
- */
-
-#include <stdio.h>
-#include "queue.h"
-#include "filterTest.h"
+#include "runningModes.h"
 #include "filter.h"
-#include "transmitter.h"
-#include "trigger.h"
-#include "lockoutTimer.h"
-#include "supportFiles/utils.h"
-#include "hitLedTimer.h"
-
-
-
-#include "supportFiles/interrupts.h"
+#include "filterTest.h"
 #include "supportFiles/buttons.h"
-
-int main()
-{
-    interrupts_initAll(true);               // main interrupt init function.
-    interrupts_enableTimerGlobalInts();     // enable global interrupts.
-    interrupts_startArmPrivateTimer();      // start the main timer.
-    interrupts_enableArmInts();             // now the ARM processor can see interrupts.
-    printf("trigger\n");
-    trigger_runTest();
-    while (1){
-        printf("continuous\n");
-        transmitter_init();
-        transmitter_setContinuousMode(true);
-        transmitter_runTest();
-        printf("non-continuous\n");
-        transmitter_init();
-        transmitter_setContinuousMode(false);
-        transmitter_runTest();
-        printf("hitLedTimer\n");
-        if (buttons_read() & BUTTONS_BTN2_MASK){
-            hitLedTimer_runTest();
-        }
-        if (buttons_read() & BUTTONS_BTN1_MASK){
-            printf("lockoutTimer\n");
-            lockoutTimer_runTest();
-        }
-    }
+#include <assert.h>
+ 
+#define BUTTONS_BTN2_MASK 0x4   // Bit mask for BTN2
+ 
+// The program comes up in continuous mode.
+// Hold BTN2 while the program starts to come up in shooter mode.
+int main() {
+  buttons_init();  // Init the buttons.
+    if (buttons_read() & BUTTONS_BTN2_MASK) // Read the buttons to see if BTN2 is drepressed.
+      runningModes_shooter();               // Run shooter mode if BTN2 is depressed.
+    else
+      runningModes_continuous();            // Otherwise, go to continuous mode.
 }
